fe1976e6780d4b4ddfc605f66e31c71e6d172810
Andrejs <andrejs.zuckovs@gmail.com>
2024-03-04 21:24:49 +0200

    Pabeigts MD

--- Day-03-MD/src/Main.java
+++ Day-03-MD/src/Main.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadi tekstu!");
+        String text = sc.nextLine();
+        int textLen = text.length();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Ievadītā teksta garums ir: " + textLen);
+
+        if(textLen > 5){
+            System.out.println("Programmēt ir forši!");
         }
+        else{
+            System.out.println("Teksts ir īsāks vai vienāds par 5 simboliem, bet Programmēt ir forši tik un tā!");
+        }
+
     }
 }
