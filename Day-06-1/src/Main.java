0e893ad1b9c8be0365ee76de013d3b0691e9a6df
Andrejs <andrejs.zuckovs@gmail.com>
2024-03-11 20:37:49 +0200

    Pielikts Uzdevums 2

--- Day-06-1/src/Main.java
+++ Day-06-1/src/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        task1();
+        //task1();
+        task2();
     }
     //------------------------------------------------------------------------------------------------------------------
     public static void task1(){
@@ -22,6 +23,23 @@ public class Main {
         else{
             System.out.println("zaudēji");
         }
+    }
+    //------------------------------------------------------------------------------------------------------------------
+    public static void task2(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Lūdzu ievadi Skaitli: ");
+        int inNumber = sc.nextInt();
+
+        if((inNumber > 10) || (inNumber < 5)) {
+            System.out.println("$$");
+        }else if((inNumber >= 6) && (inNumber <= 9)){
+            System.out.println("$");
+        }
+        else{
+            System.out.println(" Skaitlis ir 5 ");
+        }
+
     }
     //------------------------------------------------------------------------------------------------------------------
 }
