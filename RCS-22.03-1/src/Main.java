b00e0110c414cffac7a64025c70d65ce163a1e57
Andrejs <andrejs.zuckovs@gmail.com>
2024-03-22 21:14:39 +0200

    uzdevums3

--- RCS-22.03-1/src/Main.java
+++ RCS-22.03-1/src/Main.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // uzdevums1();
-        uzdevums2();
+        //uzdevums1(1);
+        //uzdevums2(2);
+        //teorija1(1);
+        uzdevums3(3);
     }
     //------------------------------------------------------------------------------------------------------------------
-    public static void uzdevums1(){
-        System.out.println("22-Marts: Uzdevums 1 - Skaitļa faktoriāls");
+    public static void uzdevums1(int Nr){
+        System.out.println("\u001B[33m 22-Marts:  ✏\uFE0F\u001B[37m-"+Nr+"- Skaitļa faktoriāls");
         Scanner sc = new Scanner(System.in);
         int inNumber = 0;
         int faktorials = 1;
@@ -31,25 +33,58 @@ public class Main {
         }
     }
     //------------------------------------------------------------------------------------------------------------------
-    public static void uzdevums2(){
-        System.out.println("22-Marts: Uzdevums 2 - Skaitļa kāpināšana");
+    public static void uzdevums2(int Nr){
+        System.out.println("\u001B[33m 22-Marts:  ✏\uFE0F\u001B[37m-"+Nr+" - Skaitļa kāpināšana");
         Scanner sc = new Scanner(System.in);
         int X = 0;
         int pow = 0;
         int result = 1;
 
-        System.out.println("Lūdzu ievadi skaitli X: ");
+        System.out.print("Lūdzu ievadi skaitli X: ");
         X = sc.nextInt();
-        System.out.println("Lūdzu ievadi skaitļa " + X + " kāpnātāju :");
+        System.out.print("Lūdzu ievadi skaitļa " + X + " kāpnātāju :");
         pow = sc.nextInt();
-        
+
         for(int i = 0; i < pow; i++){
             result = result * X;
         }
 
-        System.out.println("------------------------------");
-        System.out.println("Rezultāts: " );
-        System.out.println(X + "^" + pow+"="+result);
+        System.out.print("Rezultāts: " );
+        System.out.println("┈➤ "+"\u001B[31m" + X + "^" + pow+"="+result);
+        System.out.println("┈➤ "+"\u001B[31m" + "END");
     }
+    //------------------------------------------------------------------------------------------------------------------
+    public static void teorija1(int Nr){
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        while(!input.equals("exit")){
+            System.out.println("Hello");
+            System.out.print("Lūdzu ievadi skaitli tekstu: ");
+            input = sc.nextLine();
+        }
+        System.out.println("┈➤ "+"\u001B[31m" + "END");
+    }
+    //------------------------------------------------------------------------------------------------------------------
+    public static void uzdevums3(int Nr){
+        System.out.println("\u001B[33m 22-Marts:  ✏\uFE0F\u001B[37m-"+Nr+" - while cikls");
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+        String input = "";
+        int X = 0;
+        int SUM = 0;
 
+        while(!input.equals("jā")){
+            System.out.print("Lūdzu ievadi skaitli X: ");
+            X = sc1.nextInt();
+            SUM = SUM+X;
+            System.out.print("Vai iziet: ");
+            input = sc2.nextLine();
+        }
+
+        System.out.print("Rezultāts: " );
+        System.out.println("┈➤ "+"\u001B[31m" + SUM);
+        System.out.println("┈➤ "+"\u001B[31m" + "END");
+
+    }
+    //------------------------------------------------------------------------------------------------------------------
 }
