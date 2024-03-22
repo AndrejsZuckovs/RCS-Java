3945df34bdcc8ab9312cbf1a5b31270e82380316
Andrejs <andrejs.zuckovs@gmail.com>
2024-03-22 19:49:47 +0200

    Update repository Folders

--- RCS-22.03-1/src/Main.java
+++ RCS-22.03-1/src/Main.java
@@ -12,10 +12,8 @@ public class Main {
         int inNumber = 0;
         int faktorials = 1;
 
-
         System.out.println("Lūdzu ievadi pozitīvu skaitli: ");
         inNumber = sc.nextInt();
-
 
         if(inNumber <= 0){
             System.out.println("Skaitlis ir <= 0 ! Mēģini vēlreiz citreiz :( ");
@@ -23,13 +21,12 @@ public class Main {
         else{
             System.out.println("Skaitļa " + inNumber+ " faktoriāls būs: ");
             for(int i = 1; i < inNumber+1; i++){
-
                 faktorials = faktorials * i;
                 System.out.println("F [" + i + "] : " + faktorials);
-
             }
             System.out.println("------------------------------");
             System.out.println("Rezultāts ir: " + faktorials);
         }
     }
+    //------------------------------------------------------------------------------------------------------------------
 }
