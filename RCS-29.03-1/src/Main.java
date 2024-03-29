30cad8a80f2dbf8135c8d1a0b3439f4951f37cbf
Andrejs <andrejs.zuckovs@gmail.com>
2024-03-29 20:53:06 +0200

    Uzdevums 2

--- RCS-29.03-1/src/Main.java
+++ RCS-29.03-1/src/Main.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //teorija1(1,2);
-        //teorija2("Hello!");
-        uzdevums1("Hello ","world ", "!");
+        // teorija1(1,2);
+        // teorija2("Hello!");
+        // uzdevums1("Hello ","world ", "!");
+        uzdevums2(5);
     }
     //------------------------------------------------------------------------------------------------------------------
     public static void teorija1(int a, int b){
@@ -15,10 +18,22 @@ public class Main {
     }
     //------------------------------------------------------------------------------------------------------------------
     public static void uzdevums1(String a,String b,String c){
-        String text = a + b +c;
+        String text = a + b + c;
         System.out.println(text);
     }
     //------------------------------------------------------------------------------------------------------------------
+    public static void uzdevums2(int j){
+        Scanner sc = new Scanner(System.in);
+        int inNumber = 0;
+        int summ = 0;
+        System.out.println("Tev kopā būs jāievada " +j + " skaitļi.");
+        for (int i = 1; i<j+1;i++){
+            System.out.print("Iebvadi " +i+ ". skaitli: " );
+            inNumber = sc.nextInt();
+            summ +=inNumber;
+        }
+        System.out.println("Skaitļu summa ir: " +summ);
+    }
     //------------------------------------------------------------------------------------------------------------------
     //------------------------------------------------------------------------------------------------------------------
 }
