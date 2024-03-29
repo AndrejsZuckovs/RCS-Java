bd112e05b35c5aeaca86fa249370e397ee066f26
Andrejs <andrejs.zuckovs@gmail.com>
2024-03-29 21:28:43 +0200

    Uzdevums 2 - uzlabots

--- RCS-29.03-1/src/Main.java
+++ RCS-29.03-1/src/Main.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\u001B[33m 29-Marts:  ✏\uFE0F\u001B[37m"+" - Metodes ar parametriem");
+        System.out.println("\uFE0F\u001B[37m"+"----------------------------------------------------------------------");
+        //--------------------------------------------------------------------------------------------------------------
         // teorija1(1,2);
         // teorija2("Hello!");
         // uzdevums1("Hello ","world ", "!");
         uzdevums2(5);
+        //--------------------------------------------------------------------------------------------------------------
+        System.out.println("\uFE0F\u001B[37m"+"----------------------------------------------------------------------");
+        System.out.println("┈➤ "+"\u001B[31m" + "END");
     }
     //------------------------------------------------------------------------------------------------------------------
     public static void teorija1(int a, int b){
@@ -24,6 +30,8 @@ public class Main {
     //------------------------------------------------------------------------------------------------------------------
     public static void uzdevums2(int j){
         Scanner sc = new Scanner(System.in);
+        int sPara = 0;
+        int sNepara = 0;
         int summ = 0;
         int inMumber = 0;
 
@@ -34,19 +42,31 @@ public class Main {
                 inMumber = sc.nextInt();
                 if(inMumber % 2 == 0) {
                     System.out.print(i + ". ievadītais skaitlis :" + inMumber + " ir PĀRA skaitlis" + "\n");
-                    summ += inMumber;
+                    sPara += inMumber;
                 }
-                else{
-                    System.out.print(i + ". ievadītais skaitlis :" + inMumber + " ir NEPĀRA skaitlis"+ "\n");
+                else {
+                    System.out.print(i + ". ievadītais skaitlis :" + inMumber + " ir NEPĀRA skaitlis" + "\n");
+                    sNepara += inMumber;
                 }
-
-
             }
-            System.out.println("Ievadīto skaitļu summa ir: " + summ);
+            System.out.println("Ievadīto PĀRA skaitļu summa ir: " + sPara);
+            System.out.println("Ievadīto NEPĀRA skaitļu summa ir: " + sNepara);
+            summ = sPara + sNepara;
+            System.out.println("Ievadīto visu skaitļu summa ir: " + summ);
+
         }
         else if(j==1){
             System.out.print("Ievadi 1x skaitli: ");
-            System.out.println("Ievadītais skaitlis ir: "+sc.nextInt() );
+            inMumber = sc.nextInt();
+
+            if(inMumber % 2 == 0) {
+                System.out.print("Ievadītais skaitlis :" + inMumber + " ir PĀRA skaitlis" + "\n");
+            }
+            else {
+                System.out.print("Ievadītais skaitlis :" + inMumber + " ir NEPĀRA skaitlis" + "\n");
+
+            }
+
         }
         else{
             System.out.println("Tev nebūs jāievada skaitļi! Paldies :)");
