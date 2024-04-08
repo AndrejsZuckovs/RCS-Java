d8d95c056adbf5b5358f26e7b28d8066ee291c13
Andrejs <andrejs.zuckovs@gmail.com>
2024-04-08 19:20:44 +0300

    Uzdevums 1.

---
+++ RCS-08.04-1/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// metodes
+// klases
+// Scanner sc = new Scanner(System.in);
+// sc0.nextInt();       -> int
+// sc1.nextLine();      -> String
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\u001B[33m 08-Aprīlis:  ✏\uFE0F\u001B[37m-"+"- ");
+        System.out.println("\u001B[31m"+"┈➤ "  + "START");
+        System.out.print("\u001b[37m\n");
+        //--------------------------------------------------------------------------
+        //teorija1();
+        boolean result = uzdevums1();
+        System.out.println(result);
+        //--------------------------------------------------------------------------
+        System.out.println("\u001B[31m"+"┈➤ " + "END");
+    }
+    //------------------------------------------------------------------------------------------------------------------
+    public static void teorija1(){
+
+    }
+    //------------------------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
+    public static boolean uzdevums1(){
+        Scanner sc = new Scanner(System.in);
+        int number = 0;
+
+        for(int i=0;i<5;i++){
+            System.out.print("Lūdzu ievai skaitli: ");
+            number = sc.nextInt();
+            if(number == 28){
+                return true;
+            }
+        }
+        return false;
+    }
+    //------------------------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
+}
